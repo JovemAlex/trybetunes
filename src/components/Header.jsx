cbd5a170514f5e04df936077c3a417864349f4df
Alex <alexsandrolima991@hotmail.com>
2022-08-01 20:34:17 -0300

    Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -24,6 +25,15 @@ export default class Header extends Component {
         { user && (
           <p data-testid="header-user-name"><strong>{ user.name }</strong></p>
         ) }
+        <Link to="/search" data-testid="link-to-search">
+          <li>Search</li>
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          <li>Favorites</li>
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          <li>Profile</li>
+        </Link>
       </header>
     );
   }
