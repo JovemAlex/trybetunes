ad4714308d44ba534f5a0a12080c0a4459024b7d
Alex <alexsandrolima991@hotmail.com>
2022-08-01 21:03:03 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      button: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const two = 2;
+    const { value } = target;
+    if (value.length >= two) {
+      this.setState({
+        button: false,
+      });
+    } else {
+      this.setState({
+        button: true,
+      });
+    }
+  }
+
   render() {
+    const { button } = this.state;
     return (
       <div data-testid="page-search">
         Search
         <Header />
+        <form action="">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ button }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
